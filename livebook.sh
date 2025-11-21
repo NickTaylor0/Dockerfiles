f388a1eabe0aeabd7a8c8b166b82b8989800f003
Nick Taylor <nick@nicktaylor.ca>
2025-11-21 07:31:18 -0500

    add http proxy to tailscale tun

--- livebook.sh
+++ livebook.sh
@@ -1,4 +1,4 @@
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 /app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=livebook
 echo Tailscale started
 ALL_PROXY=socks5://localhost:1055/ /app/bin/server
